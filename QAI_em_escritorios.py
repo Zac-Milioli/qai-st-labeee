076888bd23276a201a2b687e106544148414baf3
Zac-Milioli <zacmilioli@gmail.com>
2023-11-23 10:30:30 -0300

    Ajustes e adições

--- QAI_em_escritorios.py
+++ QAI_em_escritorios.py
@@ -1,4 +1,10 @@
 
+class Person:
+    def __init__(self):
+        self.email: str = 'No email'
+        self.name: str = 'No name'
+
+
 class PeR:
     def __init__(self):
         self.id_pergunta: list = None
@@ -10,10 +16,9 @@ class PeR:
         dataframeable = {'ID': self.id_pergunta, 'Perguntas': self.pergunta, 'Respostas': self.resposta}
         self.json_to_send = dataframeable
 
-    def mail_to_luiza(self):
-        pass
-
-class Person:
-    def __init__(self):
-        self.email: str = 'No email'
-        self.name: str = 'No name'
+    def mail_to_luiza(self, person: Person):
+        # Email Luiza: luizatdecastro@gmail.com
+        name = person.name
+        person_email = person.email
+        title = f'RESPOSTAS QAI DE {name}'
+        body_text = self.json_to_send
