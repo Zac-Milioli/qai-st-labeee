4ab7e8a9a26b189366c35c1bceae94098ac52e37
Zac-Milioli <zacmilioli@gmail.com>
2023-12-11 14:41:10 -0300

    removido print inutil

--- QAI.py
+++ QAI.py
@@ -1,7 +1,5 @@
 from configurate import *
 
-print(PeR)
-
 create_top(big_text_title='QAI em Escritórios', subtitle='Design e organização: Luiza de Castro', subtitle2='Desenvolvimento: Zac Milioli')
 
 st.markdown('***Caso a barra lateral (sidebar) esteja aberta, sinta-se a vontade para fecha-la. Ela serve apenas para configuração do questionário**')
