ee885ac0917e2577710536c608aba25d61d6e2be
Zac-Milioli <zacmilioli@gmail.com>
2023-12-11 16:55:18 -0300

    removidos frufrus

--- QAI.py
+++ QAI.py
@@ -1,7 +1,3 @@
-import os
-os.system('pip install streamlit_extras')
-os.system('pip install st_pages')
-os.system('pip install email')
 from configurate import *
 
 create_top(big_text_title='QAI em Escritórios', subtitle='Design e organização: Luiza de Castro', subtitle2='Desenvolvimento: Zac Milioli')
