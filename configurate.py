ce613cdfcb994d2b0bc5b0f8819e73054ce27f8a
Zac-Milioli <zacmilioli@gmail.com>
2023-12-15 11:02:55 -0300

    Barra lateral escondidinha :)

--- configurate.py
+++ configurate.py
@@ -15,6 +15,15 @@ def create_top(big_text_title: str = None, subtitle: str = None, subtitle2: str 
     layout='wide',
     initial_sidebar_state='collapsed'
     )
+    st.markdown(
+    """
+<style>
+    [data-testid="collapsedControl"] {
+        display: none
+    }
+</style>
+""",
+    unsafe_allow_html=True,)
     hide_pages(['QAI', 'q0', 'introq1_sat', 'introq1_insat', 'q1a', 'c0', 'q1b', 'q1c', 'q2', 'q2a', 'q3', 'q3a', 'q4', 'q4a', 'q5', 'q5a', 'hi', 'cg', 'cp', 'sp', 'q6', 'fim'])
     with st.container():
         esquerda, meio, direita = st.columns(3)
