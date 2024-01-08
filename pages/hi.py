41a74cb77bee683249906894f384f21da22c4e3a
Zac-Milioli <zacmilioli@gmail.com>
2024-01-08 15:40:12 -0300

    removido match case

--- pages/hi.py
+++ pages/hi.py
@@ -12,34 +12,33 @@ with st.container():
     while len(selecao) >= 1:
         indice_sorteado = randint(0,len(selecao)-1)
         valor_sorteado = selecao[indice_sorteado]
-        match valor_sorteado:
-            case 1:
-                fat1 = create_radio(phrase='conforto térmico', divide=True, extreme_left=left, extreme_right=right, min=1, max=5, key='fat1')
-                selecao.pop(indice_sorteado)
-            case 2:
-                fat2 = create_radio(phrase='qualidade do ar', divide=True, extreme_left=left, extreme_right=right, min=1, max=5, key='fat2')
-                selecao.pop(indice_sorteado)
-            case 3:
-                fat3 = create_radio(phrase='conforto visual', divide=True, extreme_left=left, extreme_right=right, min=1, max=5, key='fat3')
-                selecao.pop(indice_sorteado)
-            case 4:
-                fat4 = create_radio(phrase='conforto acústico', divide=True, extreme_left=left, extreme_right=right, min=1, max=5, key='fat4')
-                selecao.pop(indice_sorteado)
-            case 5:
-                fat5 = create_radio(phrase='ambientes específicos para atividades diferenciadas', divide=True, extreme_left=left, extreme_right=right, min=1, max=5, key='fat5')
-                selecao.pop(indice_sorteado)
-            case 6:
-                fat6 = create_radio(phrase='proximidade e/ou acesso a vistas externas', divide=True, extreme_left=left, extreme_right=right, min=1, max=5, key='fat6')
-                selecao.pop(indice_sorteado)
-            case 7:
-                fat7 = create_radio(phrase='privacidade visual', divide=True, extreme_left=left, extreme_right=right, min=1, max=5, key='fat7')
-                selecao.pop(indice_sorteado)
-            case 8:
-                fat8 = create_radio(phrase='privacidade acústica', divide=True, extreme_left=left, extreme_right=right, min=1, max=5, key='fat8')
-                selecao.pop(indice_sorteado)
-            case 9:
-                fat9 = create_radio(phrase='estar próximo à colegas e equipe de trabalho mesmo que não esteja totalmente confortável', divide=True, extreme_left=left, extreme_right=right, min=1, max=5, key='fat9')
-                selecao.pop(indice_sorteado)
+        if valor_sorteado == 1:
+            fat1 = create_radio(phrase='conforto térmico', divide=True, extreme_left=left, extreme_right=right, min=1, max=5, key='fat1')
+            selecao.pop(indice_sorteado)
+        elif valor_sorteado == 2:
+            fat2 = create_radio(phrase='qualidade do ar', divide=True, extreme_left=left, extreme_right=right, min=1, max=5, key='fat2')
+            selecao.pop(indice_sorteado)
+        elif valor_sorteado == 3:
+            fat3 = create_radio(phrase='conforto visual', divide=True, extreme_left=left, extreme_right=right, min=1, max=5, key='fat3')
+            selecao.pop(indice_sorteado)
+        elif valor_sorteado == 4:
+            fat4 = create_radio(phrase='conforto acústico', divide=True, extreme_left=left, extreme_right=right, min=1, max=5, key='fat4')
+            selecao.pop(indice_sorteado)
+        elif valor_sorteado == 5:
+            fat5 = create_radio(phrase='ambientes específicos para atividades diferenciadas', divide=True, extreme_left=left, extreme_right=right, min=1, max=5, key='fat5')
+            selecao.pop(indice_sorteado)
+        elif valor_sorteado == 6:
+            fat6 = create_radio(phrase='proximidade e/ou acesso a vistas externas', divide=True, extreme_left=left, extreme_right=right, min=1, max=5, key='fat6')
+            selecao.pop(indice_sorteado)
+        elif valor_sorteado == 7:
+            fat7 = create_radio(phrase='privacidade visual', divide=True, extreme_left=left, extreme_right=right, min=1, max=5, key='fat7')
+            selecao.pop(indice_sorteado)
+        elif valor_sorteado == 8:
+            fat8 = create_radio(phrase='privacidade acústica', divide=True, extreme_left=left, extreme_right=right, min=1, max=5, key='fat8')
+            selecao.pop(indice_sorteado)
+        elif valor_sorteado == 9:
+            fat9 = create_radio(phrase='estar próximo à colegas e equipe de trabalho mesmo que não esteja totalmente confortável', divide=True, extreme_left=left, extreme_right=right, min=1, max=5, key='fat9')
+            selecao.pop(indice_sorteado)
 
 
 st.title('')
