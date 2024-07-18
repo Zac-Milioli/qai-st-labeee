cd8c5561ff4aac147c2f27b11c19495a828d3242
Zac-Milioli <zacmilioli@gmail.com>
2024-07-18 14:08:17 -0300

    pequenos negócios grandes mudanças

--- QAI.py
+++ QAI.py
@@ -5,40 +5,72 @@ create_top(big_text_title='QAI em Escritórios')
 st.info('Para garantir a autenticidade da pesquisa, será enviado um código de verificação para seu email. **Suas respostas são anônimas e confidenciais.**', icon='ℹ️')
 st.title('')
 
-edificio = st.text_input(label='None', label_visibility="collapsed", max_chars=8, placeholder="Código do edifício")
-col1, col2, col3 = st.columns(3)
-caixa_email, botao_enviar = st.columns([1,0.1])
-mail = caixa_email.text_input('no label', label_visibility='hidden', max_chars=100, key='emaildoparticipante', placeholder='Insira seu email')
-botao_enviar.write('')
-
-if botao_enviar.button('Enviar código de verificação'):
-    if not mail:
-        st.error('Preencha seu email para receber o código de verificação')
-    elif mail:
-        if '@' not in mail:
-            st.error('Insira um email válido para receber o código de verificação')
-        if ' ' in mail:
-            st.error('Não utilize espaços no seu endereço de email')
+form_email = st.container(border=True)
+col1, col2 = form_email.columns([4,1])
+email = col1.text_input(label="Email").strip()
+col2.subheader('')
+if col2.button(label="Enviar código de verificação", use_container_width=True):
+    try:
+        st.session_state['auth_code'] = mail_auth_code(email)
+        st.session_state['email'] = email
+    except:
+        st.error('Um erro ocorreu ao tentar enviar o email de validação. Verifique se o email inserido está correto e tente novamente', icon="⚠️")
+col1, col2 = form_email.columns(2)
+codigo = col1.text_input(label="Código de validação", max_chars=6)
+edificio = col2.text_input(label="Insira o ID do seu local de trabalho:", max_chars=8)
+check_inputs = True if not (edificio and codigo) else False
+col1, col2, col3 = form_email.columns(3)
+if col2.button(label="Validar código e iniciar questionário", use_container_width=True, disabled=check_inputs):
+    if not st.session_state.get('auth_code'):
+        st.error('ERRO: O código de validação não foi gerado', icon="⚠️")
+    elif st.session_state['auth_code'] == codigo:
+        returned, status = get_build_info_by_id(id_=int(edificio))
+        if status == "OK":
+            st.session_state['edificio'] = edificio
+            st.session_state['hierarquia'] = 0
+            st.session_state['janela'] = 0
+            switch_page('q0')
         else:
-            st.session_state['PeR']['email'] = mail
-            st.session_state['auth'] = mail_auth_code(mail_person=mail)
-
-
-esquerda, meio, direita = st.columns([1.6,1,1.8])
-confirmacao_texto = meio.text_input('nolabel', label_visibility='hidden', max_chars=10, key='codigoverificacao', placeholder='Código de verificação')
-
-st.title('')
-if centered_button('Iniciar questionário'):
-    if confirmacao_texto and edificio:
-        if st.session_state['auth']:
-            st.write(st.session_state['auth']) 
-            if confirmacao_texto != st.session_state['auth']:
-                st.error('Usuário não autorizado / Código incorreto')
-            else:
-                st.session_state['PeR']['edificio'] = edificio
-                st.session_state['hierarquia'] = 0
-                switch_page('q0')
-        else:
-            st.error('Código de verificação não gerado, insira seu email, clique no botão de envio e copie o código que receber por email')
+            st.error("O ID do local de trabalho inserido não foi encontrado na base de dados", icon='⚠️')
     else:
-        st.error('Insira todas as informações para prosseguir')
+        st.error('ERRO: O código inserido é diferente do código enviado', icon="⚠️")
+
+footer()
+
+# edificio = st.text_input(label='None', label_visibility="collapsed", max_chars=8, placeholder="Código do edifício")
+# col1, col2, col3 = st.columns(3)
+# caixa_email, botao_enviar = st.columns([1,0.1])
+# mail = caixa_email.text_input('no label', label_visibility='hidden', max_chars=100, key='emaildoparticipante', placeholder='Insira seu email')
+# botao_enviar.write('')
+
+# if botao_enviar.button('Enviar código de verificação'):
+#     if not mail:
+#         st.error('Preencha seu email para receber o código de verificação')
+#     elif mail:
+#         if '@' not in mail:
+#             st.error('Insira um email válido para receber o código de verificação')
+#         if ' ' in mail:
+#             st.error('Não utilize espaços no seu endereço de email')
+#         else:
+#             st.session_state['PeR']['email'] = mail
+#             st.session_state['auth'] = mail_auth_code(mail_person=mail)
+
+
+# esquerda, meio, direita = st.columns([1.6,1,1.8])
+# confirmacao_texto = meio.text_input('nolabel', label_visibility='hidden', max_chars=10, key='codigoverificacao', placeholder='Código de verificação')
+
+# st.title('')
+# if centered_button('Iniciar questionário'):
+#     if confirmacao_texto and edificio:
+#         if st.session_state['auth']:
+#             st.write(st.session_state['auth']) 
+#             if confirmacao_texto != st.session_state['auth']:
+#                 st.error('Usuário não autorizado / Código incorreto')
+#             else:
+#                 st.session_state['PeR']['edificio'] = edificio
+#                 st.session_state['hierarquia'] = 0
+#                 switch_page('q0')
+#         else:
+#             st.error('Código de verificação não gerado, insira seu email, clique no botão de envio e copie o código que receber por email')
+#     else:
+#         st.error('Insira todas as informações para prosseguir')
--- configurate.py
+++ configurate.py
@@ -4,14 +4,46 @@ import streamlit as st
 import smtplib
 import email.message
 from random import randint
+import json
+import requests
+from streamlit_gsheets import GSheetsConnection
+import gspread
+from google.oauth2.service_account import Credentials
+from time import sleep
+from datetime import datetime
+
 
 st.session_state['PeR'] = {}
 placeholder_img = r'static/placeholder.png'
 st.session_state['auth'] = False
+# ESTA VARIÁVEL É SENSÍVEL E NA APLICAÇÃO REAL DEVE SER ADICIONADA AO ENV DO STREAMLIT E ACESSADA ATRAVÉS DE st.secrets["CREDENTIALS"]
+credentials = {"type": "service_account","project_id": "sheets-connection-api","private_key_id": "c693b8107c458cd42c7f0be9ffc706aa6eff4dbd","private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDDh3+tWRN886tG\nkrOEBRbMxvFAQITqV8FefzkoZVkTX31Gad+yTgd/T7phnU/mcUcm9vNRjbRfBXqU\nOC+Qle9kldqJGOzhA2IvwGLHnN5+jVUXAl9Nj6C8Fs3qdiZfwEXnC62APIo9QYrR\nH0/u+ixkPFzPEu/3Ny8ECagKSt8hIxCAHU1Fe383XfiWHRebu/wdxlvBNL/Erlnf\nTygY0Cdm3ntMJbAdXRDDtO1sbL8y2Y7dsJNh5yex7kOFF7CgGWoZoiXwx4ujikd+\n1l6QG+So8nxp7pf1e/T3RA2Tfby47MjON/MWEF4p4XqGLt7zt3bkZJQagwuAeAD2\nGsjFNwMLAgMBAAECggEAF4eCO0uJV1OBTGxR8vSj0j+sf3VkKUknKWZ4694KUpT+\nn88UzsGqEE15Wc7S8yG1lMBJ9ontZzPjFNwQCT+pv7pywmW/97HvCl0F5gyAD82V\nLEAqVMyQZKH/5JOUOZiLMOsMVW6zmfx9pn20okbzuZoz9u1J+D9pRnyc/qQU5sk5\nn0i/GbHUQUwfWo2RsJ0zjvOz5n5Xc0lawvUIa04ugQfz67leDQpAkjLdw/09C3d7\nEASfL7eYI7FsAjP5HtRbS5Yd6TO4OzOp8aGJTZn+PBhjgy8gidFHKGVycecB/B5L\nyFVsV+MUqFZfq+2Vt6iV/gQsEdaMf1uILkULKXBPRQKBgQD/DlxsKkYZSAUwVnrT\n86kWXQRPjSJ5U9yGvt1Skd/x/LtWWDdNB2FVSauSSJhK+LZHlCiWc7o0gqFvq2ok\nc3zmAEN3FBUGXJXcIECtaLGgcK1XeujcpqnAhKoAsl5Z4HTCN9U7G3/7jHgmF97k\nbhR3Eu2c0WVth/RyNRutU2QNhwKBgQDEQL4Gy3zgvsoxxaZQYAf8McbwEepAzfzZ\nfr/KxY0pWT4zptgkhW0+QwSaCXShISzBiJy9aQu7Mn7XuawkKTADO428FFSZJUOe\nToR1JWzmrpGJYWR+M+asCvfVfYkEL8MoqrMBCZAwYobq07X3AiU+BohH5qi3Povq\nO+75/p9fXQKBgQDkjZLmXm9YYkA8E5KXcaXYY1vkiV0WCb74g/pB7nQWHVomQoCz\npuRij9SODj1iGUMGG07Pmz6FpXVSYvGHXnHSjPPntfgtLjQgAErU2ZcqZS3/0STv\n7Oz6libc3vlLYqZeD7gk8jyaRkK4J/XVDouKNEz2lHFmWEkFOm/lvm9O6wKBgHFr\nBSdCJJpySMI4+uQKi7LZRaJqiBoJsa40jTzvrKQP2l3Zd6KrpbXM33TyMAAK/yWe\nAQ+KDOiTxzB/Mpf3YbMMkN34Vefn3Es6D1zwUx6CFsPxkDVLY21cLVypXy0XOU9g\nT3EzCKyd1GEUF154U/OjrND44dp9ADlPh83ctFhVAoGBAMv3ly9loXruAqF2DC+8\nQkyrCI5hsNz9H2uuMhYRaCA9iWGPGr8ISrrlgYnu//mIDQ9MDJiwDBCYTQi081XU\nEM/eANKnpvGzdfQ2+9tesYM7gFH58joeZNQaXbSGSusQol7m/4CqG7VbUz7mAxqq\n0TfslpSbMgx3BmKNqwpJue3Y\n-----END PRIVATE KEY-----\n","client_email": "sheet-bot-api@sheets-connection-api.iam.gserviceaccount.com","client_id": "103080225992158614942","auth_uri": "https://accounts.google.com/o/oauth2/auth","token_uri": "https://oauth2.googleapis.com/token","auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs","client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/sheet-bot-api%40sheets-connection-api.iam.gserviceaccount.com","universe_domain": "googleapis.com"}
+
+with open("credentials.json", "w") as file:
+    json.dump(credentials, file, indent=2)
+scopes = ["https://www.googleapis.com/auth/spreadsheets"]
+creds = Credentials.from_service_account_file(r"credentials.json", scopes=scopes)
+client = gspread.authorize(creds)
+
+# ESTA VARIÁVEL É SENSÍVEL E NA APLICAÇÃO REAL DEVE SER ADICIONADA AO ENV DO STREAMLIT E ACESSADA ATRAVÉS DE st.secrets["SHEET_ID"]
+sheet_id = "1T3D_x6qr4OjXQ6qMXJR0ZCywifJaHP1ck-PbZ2vT_2I"
+
+workbook = client.open_by_key(sheet_id)
+worksheet_build = workbook.worksheet('build')
+worksheet_user = workbook.worksheet('user')
+
+support_mail = "escritorios.qai.bot@gmail.com"
+
+# ESTA VARIÁVEL É SENSÍVEL E NA APLICAÇÃO REAL DEVE SER ADICIONADA AO ENV DO STREAMLIT E ACESSADA ATRAVÉS DE st.secrets['EMAIL_PASSWORD']
+password = "sxux ztfv fsiw aqfp"
 
 # AUTH_LIST = ["519249","215333","885531","618511","572447","869808","795177","371314","349847","725307","152101","687085","930430","862644","631116","202600","226685","675900","850068","502866","499124","119076","194394","850663","501970","343110","604899","873815","873732","332027","229599","195539","642693","113458","973402","798801","651854","588405","940423","774287","620429","797880","652906","713421","149751","419936","947997","372791","250261","469589","499024","854435","413249","856289","381478","617604","476102","738816","105552","961946","633041","305899","566873","299503","924671","502313","124981","563472","999154","314874","205129","685368","875393","430502","433164","209055","254352","479366","755502","800045","218490","280180","666648","651533","831232","711813","665228","484822","798532","145640","886177","791129","482785","486436","611493","464165","665697","786134","344894","828499"]
 # authorization_list = st.secrets["AUTH_LIST"]
 authorization_list = ["519249","215333","885531","618511","572447","869808","795177","371314","349847","725307","152101","687085","930430","862644","631116","202600","226685","675900","850068","502866","499124","119076","194394","850663","501970","343110","604899","873815","873732","332027","229599","195539","642693","113458","973402","798801","651854","588405","940423","774287","620429","797880","652906","713421","149751","419936","947997","372791","250261","469589","499024","854435","413249","856289","381478","617604","476102","738816","105552","961946","633041","305899","566873","299503","924671","502313","124981","563472","999154","314874","205129","685368","875393","430502","433164","209055","254352","479366","755502","800045","218490","280180","666648","651533","831232","711813","665228","484822","798532","145640","886177","791129","482785","486436","611493","464165","665697","786134","344894","828499"]
+
+# ESTA VARIÁVEL É SENSÍVEL E NA APLICAÇÃO REAL DEVE SER ADICIONADA AO ENV DO STREAMLIT E ACESSADA ATRAVÉS DE st.secrets["INJECTION"]
+injection = ['insert', 'drop', 'create', 'select', '*', 'update', 'delete', 'alter', 'truncate', 'execute', 'union', '--', '#', ';', 'true', 'false']
+
 
 def create_top(big_text_title: str = None, subtitle: str = None, subtitle2: str = None, subsubtitle: str = None, img_url: str = None, use_line: bool = True, use_progress: bool = False, progress_percentage:int = 0):
     st.set_page_config(
@@ -41,10 +73,6 @@ def create_top(big_text_title: str = None, subtitle: str = None, subtitle2: str 
             meio.progress(progress_percentage)
 
     with st.container():
-        esquerda, meio, direita = st.columns([1.1,1, 1])
-        meio.image(r'static/lab_banner.png', width=400)
-
-    with st.container():
         esquerda, direita = st.columns(2, gap='medium')
         if big_text_title:
             esquerda.title(big_text_title)
@@ -59,9 +87,41 @@ def create_top(big_text_title: str = None, subtitle: str = None, subtitle2: str 
     if use_line:
         st.markdown('---')
 
+
+def footer():
+    st.title('')
+    st.title('')
+    st.title('')
+    col1, _, col2 = st.columns([0.5,1.5,0.5])
+    col1.image(r'static/lab_banner.png', width=300)
+    col2.markdown("")
+    col2.caption(f"Dúvidas e suporte: {support_mail}")
+
+
+def check_for_injection(items: list):
+    for item in items:
+        if item in injection:
+            return "INJECTION"
+    return "OK"
+
+
+def get_build_info_by_id(id_: int):
+    conn = st.connection("gsheets", type=GSheetsConnection, ttl=0)
+    sql = f'SELECT * FROM build WHERE "id" = {id_}'
+    response = conn.query(sql)
+    if response.empty:
+        return None, "ERROR"
+    return response, "OK"
+
+
+def register_answer():
+    worksheet_user.append_row([st.session_state['edificio']] + [st.session_state['email']] + list(st.session_state['PeR'].values()))
+
+
 def place_left_subtitle(text: str):
     esquerda, direita = st.columns([3,1])
     esquerda.markdown(f'**{text}**')
+
 
 def next_page_button(name: str, phrase: str = None, aviso: str = None):
     esquerda, direita = st.columns([4,1])
@@ -73,10 +133,12 @@ def next_page_button(name: str, phrase: str = None, aviso: str = None):
     botao = direita.button(label=name)
     return botao
 
+
 def centered_button(name: str):
     esquerda, meio, direita = st.columns([1.5,1,1])
     botao = meio.button(name)
     return botao
+
 
 def create_radio(name: str = None, phrase:str = None, extreme_left:str = None, extreme_right:str = None, min:int = 0, max:int = 10, divide:bool = False, key = None, show_values:bool = False, large:bool = False, five_columns_width:list = [1,0.5,0.4,0.6,1], two_columns_width:list = [1,2], selection:list = None, use_list_selection:bool = False, desabilitado: bool = False):
     if use_list_selection:
@@ -135,6 +197,7 @@ def create_radio(name: str = None, phrase:str = None, extreme_left:str = None, e
                 valor = st.radio(label='No name', label_visibility='hidden', options=opt, horizontal=True, format_func=(lambda x: ''), index=None, key=key)
     return valor
 
+
 def nested_radio(name: str = None, text_left:str = None, text_right:str = None, min:int = 0, max:int = 10, key = None, show_values:bool = False, columns_width:list = [0.2,0.6,0.6], selection:list = None, use_list_selection:bool = False):
     esquerda, meio, direita = st.columns(columns_width)
     if use_list_selection:
@@ -161,9 +224,6 @@ def nested_radio(name: str = None, text_left:str = None, text_right:str = None, 
             valor = meio.radio(label='No name', label_visibility='hidden', options=opt, horizontal=True, format_func=(lambda x: ''), index=None, key=key)
     return valor
 
-# EMAIL_PASSWORD = "sxux ztfv fsiw aqfp"
-# password = st.secrets['EMAIL_PASSWORD']
-password = "sxux ztfv fsiw aqfp"
 
 def mail_me(mail_person:str, perguntas_e_respostas:dict):
     corpo_email = f'{perguntas_e_respostas}'
@@ -176,6 +236,7 @@ def mail_me(mail_person:str, perguntas_e_respostas:dict):
     s.starttls()
     s.login(msg['From'], password)
     s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
+
 
 def mail_auth_code(mail_person:str):
     auth_code = authorization_list[randint(0, len(authorization_list)-1)]
@@ -198,6 +259,7 @@ def mail_auth_code(mail_person:str):
     s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
     return auth_code
 
+
 def send_thanks_email(mail_person:str):
     corpo_email = f"""
     <h2>A equipe LabEEE agradece pela participação na pesquisa!</h2>
@@ -218,7 +280,6 @@ def send_thanks_email(mail_person:str):
 
 
 ### IMPORTS FROM EXTERNAL MODULES
-
 ## STREAMLIT_EXTRAS --> switch page button
 def switch_page(page_name: str):
     """
