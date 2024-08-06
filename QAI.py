6849a10125732d5d6ab56521d8ad1882dd4cef9b
Zac-Milioli <zacmilioli@gmail.com>
2024-08-06 10:30:28 -0300

    fix: troca de palavra

--- QAI.py
+++ QAI.py
@@ -14,15 +14,15 @@ if col2.button(label="Enviar código de verificação", use_container_width=True
         st.session_state['auth_code'] = mail_auth_code(email)
         st.session_state['email'] = email
     except:
-        st.error('Um erro ocorreu ao tentar enviar o email de validação. Verifique se o email inserido está correto e tente novamente', icon="⚠️")
+        st.error('Um erro ocorreu ao tentar enviar o email de verificação. Verifique se o email inserido está correto e tente novamente', icon="⚠️")
 col1, col2 = form_email.columns(2)
-codigo = col1.text_input(label="Código de validação", max_chars=6)
+codigo = col1.text_input(label="Código de verificação", max_chars=6)
 edificio = col2.text_input(label="Insira o ID do seu local de trabalho:", max_chars=8)
 check_inputs = True if not (edificio and codigo) else False
 col1, col2, col3 = form_email.columns(3)
 if col2.button(label="Validar código e iniciar questionário", use_container_width=True, disabled=check_inputs):
     if not st.session_state.get('auth_code'):
-        st.error('ERRO: O código de validação não foi gerado', icon="⚠️")
+        st.error('ERRO: O código de verificação não foi gerado', icon="⚠️")
     elif st.session_state['auth_code'] == codigo:
         with st.spinner("Verificando código e ID de local de trabalho..."):
             returned, status = get_build_info_by_id(id_=int(edificio))
